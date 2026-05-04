0763c86a5427d14577a931fcde896afc57001a23
N00R <noureldin850@gmail.com>
2026-05-04 18:32:03 +0300

    Enhance Patient class with additional attributes and methods
    Added properties for age, gender, phone, and doctor. Implemented constructor and displayInfo method.

--- prog_final/Patient.java
+++ prog_final/Patient.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
 public class Patient extends User {
+    private int age;
+    private String gender;
+    private String phone;
+    private Doctor doctor;
+    private ArrayList<Appointment> appointments;
+
+    Patient(String ID, String name, String username, String password,int age, String gender, String phone){
+        super(ID, name, username, password);
+        this.age=age;
+        this.gender=gender;
+        this.phone=phone;
+        this.doctor=null;
+        this.appointments= new ArrayList<>();
+    }
+
+    public void setDoctor(Doctor doctor){
+        this.doctor=doctor;
+    }
     
+    @Override
+    public void displayInfo(){
+        System.out.println("ID: " + ID);
+        System.out.println("Name: " + name);
+        System.out.println("Username: " + username);        
+        System.out.println("Age: "+age);
+        System.out.println("Gender: "+gender);
+        System.out.println("Phone number: "+phone);
+    }
 }
