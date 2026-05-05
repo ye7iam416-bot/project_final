108a0a5ee54f538ea760e962908de5e263d6d5f0
ms3320227-ux <ms3320227@gmail.com>
2026-05-06 01:47:26 +0300

    اعععععع

--- prog_final/Doctor.java
+++ prog_final/Doctor.java
@@ -1,22 +1,48 @@
 public class Doctor extends User {
     String specialization;
     String department;
+    String phonenumber;
     String[] assignedPatients = new String[100]; // array ثابت
     String[] appointments = new String[100];
     int patientCount = 0;
     int appointmentCount = 0;
     
-    public Doctor(String ID, String name, String username, String password, String specialization, String department,
+    public Doctor(String ID, String name, String username, String password, String specialization, String department,String phonenumber,
             String[] assignedPatients, String[] appointments, int patientCount, int appointmentCount) {
         super(ID, name, username, password);
         this.specialization = specialization;
         this.department = department;
+        this.phonenumber =phonenumber;
         this.assignedPatients = assignedPatients;
         this.appointments = appointments;
         this.patientCount = patientCount;
         this.appointmentCount = appointmentCount;
     }
+    @Override
+public void displayInfo() {
+        System.out.println("ID: " + ID);
+        System.out.println("Name: " + name);
+        System.out.println("Username: " + username);
+        System.out.println("specialization"+specialization);
+        System.out.println("department"+department);
+        System.out.println("phonenumber"+phonenumber);   
+    }
 
+    public void viewAppointment(){
+    if (appointmentCount==0) {
+     System.out.println("No Appointment for Dr."+ name);   
+    return;
+    }
+    System.out.println("Dr."+ name +"is Appointment:");
+    for(int i=0; i< appointmentCount; i++){
+    System.out.println((i+1)+"."+appointments[i]);
+    }
+  
+   }
     
+
+
+
+   
     
 } 
