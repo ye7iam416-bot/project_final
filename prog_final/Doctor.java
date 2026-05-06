181e118be9f212b9ba43d6b35cc42bb2b3504af8
ms3320227-ux <ms3320227@gmail.com>
2026-05-06 21:00:34 +0300

    اعععععععععععع

--- prog_final/Doctor.java
+++ prog_final/Doctor.java
@@ -2,55 +2,95 @@ public class Doctor extends User {
     String specialization;
     String department;
     String phonenumber;
-    String[] assignedPatients = new String[100]; // array ثابت
-    String[] appointments = new String[100];
+    Patient[] assignedPatients = new Patient[100];// array ثابت
+   Appointment[] appointments = new Appointment[100];
     int patientCount = 0;
     int appointmentCount = 0;
     
     public Doctor(String ID, String name, String username, String password, String specialization, String department,String phonenumber,
-            String[] assignedPatients, String[] appointments, int patientCount, int appointmentCount) {
+            Patient[] assignedPatients,Appointment[] appointments, int patientCount, int appointmentCount) {
         super(ID, name, username, password);
         this.specialization = specialization;
         this.department = department;
         this.phonenumber =phonenumber;
-        this.assignedPatients = assignedPatients;
-        this.appointments = appointments;
         this.patientCount = patientCount;
         this.appointmentCount = appointmentCount;
     }
     @Override
-public void displayInfo() {
-        System.out.println("ID: " + ID);
-        System.out.println("Name: " + name);
-        System.out.println("Username: " + username);
+       public void displayInfo() {
+        super.displayInfo();
         System.out.println("specialization"+specialization);
         System.out.println("department"+department);
         System.out.println("phonenumber"+phonenumber);   
     }
 
-    public void viewAppointment(){
-    if (appointmentCount==0) {
-     System.out.println("No Appointment for Dr."+ name);   
-    return;
+   public  void addpatient(Patient p){
+    if (patientCount<assignedPatients.length) {
+     assignedPatients[patientCount++]=p;  
+        }
+  else{
+    System.out.println("Patient list is full");
     }
-    System.out.println("Dr."+ name +"is Appointment:");
-    for(int i=0; i< appointmentCount; i++){
-    System.out.println((i+1)+"."+appointments[i]);
-    }
-  
-   }
+}
 public void viewAssignedPatients() {
     if (patientCount == 0) {
         System.out.println("No patients assigned to Dr. " + name);
     } else {
         System.out.println("Assigned Patients for Dr. " + name + " :");
         for (int i = 0; i < patientCount; i++) {
-            System.out.println((i + 1) + ". Patient ID: " + assignedPatients[i]);
+            System.out.println("Patient " + (i + 1));
+             assignedPatients[i].displayInfo();
+            System.out.println("-------------------");
         }
         System.out.println("Total patients: " + patientCount);
     }
 }
-    
+
+  public void addAppointment(Appointment a) {
+        if (appointmentCount < appointments.length) {
+            appointments[appointmentCount++] = a;
+        } else {
+            System.out.println("Appointment list is full");
+        }
+    }
+
+    public void viewAppointments() {
+        if (appointmentCount == 0) {
+            System.out.println("No appointments");
+            return;
+        }
+
+        for (int i = 0; i < appointmentCount; i++) {
+            Appointment a = appointments[i];
+            System.out.println("ID: " + a.getAppointmentId());
+            System.out.println("Patient ID: " + a.getPatientId());
+            System.out.println("Date: " + a.getDate());
+            System.out.println("Time: " + a.getTime());
+            System.out.println("Status: " + a.getStatus());
+            System.out.println("-------------------");
+        }
+    }
+     public void updateAppointmentStatus(int index, String newStatus) {
+        if (index < 0 || index >= appointmentCount) {
+            System.out.println("Invalid appointment");
+            return;
+        }
+
+        appointments[index].updateStatus(newStatus);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+   
 
 
 
