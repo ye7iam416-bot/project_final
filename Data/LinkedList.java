2aa17fbd230b0069ecef6f1fab469ed3bf271a98
Moustafa amr <moustafaamrjjk@gmail.com>
2026-05-07 11:11:12 +0300

    Update LinkedList.java

--- Data/LinkedList.java
+++ Data/LinkedList.java
@@ -2,5 +2,28 @@ package Data;
 
 public class LinkedList {
     
+    Node head;
+    Node tail;
+
+    public LinkedList() {
+        head = null;
+        tail = null;
+    }
+
     
+    public void insertatfront(Node node) {
+        node.prev = null;
+        node.next = null;
+
+        
+        if (head == null) {
+            head = node;
+            tail = node;
+        }
+        else {
+            node.next = head;  
+            head.prev = node;  
+            head = node;       
+        }
+    }
 }
